74168d2208ff151eeb431d7010f4e2678f354773
Mohan261 <mohan2606201@gmail.com>
2023-11-08 18:24:35 +0530

    first deploy

--- src/components/Bookmyshow.js
+++ src/components/Bookmyshow.js
@@ -1,9 +1,11 @@
 import React from "react";
 import { Outlet } from "react-router-dom";
+import MoviesSec from "./MoviesSec";
 
 const Bookmyshow = () => {
   return (
     <div>
+      <MoviesSec />
       <Outlet />
     </div>
   );
